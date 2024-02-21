56b90d398e04b867a97901a98d903c6d63bd13b5
memet altunel <mmt.altnl@gmail.com>
2024-02-21 23:38:05 +0300

    add register module

--- src/app/_auth/auth-routing.module.ts
+++ src/app/_auth/auth-routing.module.ts
@@ -9,6 +9,10 @@ const routes: Routes = [
   {
     path: 'login',
     loadChildren: () => import('./login/login.module').then((m) => m.LoginModule)
+  },
+  {
+    path: 'register',
+    loadChildren: () => import('./register/register.module').then((m) => m.RegisterModule)
   }
 ];
 
---
+++ src/app/_auth/register/register.module.ts
@@ -0,0 +1,18 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { RegisterComponent } from './register.component';
+import { RouterModule } from '@angular/router';
+
+@NgModule({
+  imports: [
+    CommonModule,
+    RouterModule.forChild([
+      {
+        path: '', component: RegisterComponent
+      }
+    ])
+
+  ],
+  declarations: [RegisterComponent]
+})
+export class RegisterModule { }
