aa46a126719528281db66057456316086594375b
Sumeyye <sumeyyesabirli@gmail.com>
2024-02-22 17:23:27 +0300

    Şifre gizle-göster

--- src/app/_auth/login/login.component.ts
+++ src/app/_auth/login/login.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { ExternalButtonModel } from 'src/app/_layout/_models/external-button-model';
 
 @Component({
   selector: 'app-login',
@@ -8,8 +9,20 @@ import { Component, OnInit } from '@angular/core';
 export class LoginComponent implements OnInit {
 
   constructor() { }
+  passwordVisible: boolean = false;
 
-  ngOnInit() {
+  ngOnInit() {}
+
+  visible(){
+    this.passwordVisible = !this.passwordVisible;//her tıkladığımızda değeri tersine çevirir
+    debugger;
+    //getElementById metodu, password id ye sahip olan bir hmtl öğesini arıyoruz.
+    var  passwordInput = document.getElementById('password') as HTMLInputElement; // input öğesine erişeme değer ve tür değiştirme veya okuma yapabilirz
+    if (this.passwordVisible) {
+      passwordInput.type = 'text';
+    } else {
+      passwordInput.type = 'password';
+    }
   }
 
 }
