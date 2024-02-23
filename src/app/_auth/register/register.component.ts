172b8e7115b7e58b0c4008b6c2400191e437c289
Sumeyye <sumeyyesabirli@gmail.com>
2024-02-23 15:29:23 +0300

    sign up done

--- src/app/_auth/register/register.component.ts
+++ src/app/_auth/register/register.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { NgForm } from '@angular/forms';
+import { User } from 'src/app/_models/user';
+import { UserService } from 'src/app/_services/admin/user.service';
+import { NotificationService } from 'src/app/_services/notification.service';
 
 @Component({
   selector: 'app-register',
@@ -6,10 +10,34 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./register.component.css']
 })
 export class RegisterComponent implements OnInit {
+  addedUser: User = new User();
+  passwordVisible: boolean = false;
 
-  constructor() { }
+  constructor(private _service: UserService, private _notificationService: NotificationService) { }
 
   ngOnInit() {
   }
 
+  visible(){
+    this.passwordVisible = !this.passwordVisible;
+    var  passwordInput = document.getElementById('password') as HTMLInputElement;
+    if (this.passwordVisible) {
+      passwordInput.type = 'text';
+    } else {
+      passwordInput.type = 'password';
+    }
+  }
+
+  register(){
+      this._service.addUser(this.addedUser).subscribe((res) => {
+        if (res.statusCode == 200) {
+          this._notificationService.success(res.message);
+        } else {
+          this._notificationService.warning(res.message);
+        }
+      }, err => {
+        console.log(err);
+        this._notificationService.error(err);
+      });
+  }
 }
---
+++ src/app/_models/user.ts
@@ -0,0 +1,9 @@
+export class User {
+  username: string = "";
+  password: string = "";
+  mail: string = "";
+  firstName: string = "";
+  lastName: string = "";
+  description?: string = "";
+  gender: boolean = false;
+}
